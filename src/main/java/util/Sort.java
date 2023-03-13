7f678bc88f6cabe0d986090e15ae847bc7d21d87
Ekaterina K <34368106+riarurainey@users.noreply.github.com>
2023-03-13 17:11:50 +0300

    Update Sort.java
    refactoring

--- src/main/java/util/Sort.java
+++ src/main/java/util/Sort.java
@@ -6,15 +6,19 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Sort {
+public final class Sort {
 
-    public List<City> sortedByName(List<City> cities) {
+    private Sort() {
+
+    }
+
+    public static List<City> sortedByName(List<City> cities) {
         return cities.stream()
                 .sorted(Comparator.comparing(City::getName, String::compareToIgnoreCase))
                 .collect(Collectors.toList());
     }
 
-    public List<City> sortedByDistrictAndName(List<City> cities) {
+    public static List<City> sortedByDistrictAndName(List<City> cities) {
         return cities.stream()
                 .sorted(Comparator.comparing(City::getDistrict)
                         .thenComparing(City::getName))
